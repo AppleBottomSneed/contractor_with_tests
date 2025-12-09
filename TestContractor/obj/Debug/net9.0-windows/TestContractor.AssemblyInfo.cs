33689606c23b8a5786f4882629f48d30d0b80ac5
Placardo <20092387@tafe.wa.edu.au>
2025-12-09 13:15:30 +1100

    update

--- TestContractor/obj/Debug/net9.0-windows/TestContractor.AssemblyInfo.cs
+++ TestContractor/obj/Debug/net9.0-windows/TestContractor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestContractor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7efeb1a8f381aecfecbfd5d99ef1088d4edba0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestContractor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestContractor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
